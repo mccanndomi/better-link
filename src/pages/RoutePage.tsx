c55ac7b2285c30d4c919f2198eb2241eb11ef686
dominic.mccann <dominic.mccann@eyemagnet.com>
2021-11-19 12:05:34 +1300

    testing small change

--- src/pages/RoutePage.tsx
+++ src/pages/RoutePage.tsx
@@ -4,6 +4,7 @@ function RoutePage({ id }: any): JSX.Element {
   return (
     <div>
       <h1>Route: {id}</h1>
+      <h3>Below you can see the timetable for the selected route</h3>
     </div>
   );
 }
